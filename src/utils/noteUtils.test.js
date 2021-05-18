a2a37a4160df554f33090ceac4d112cd142aac99
jyi <joshua.yi@amadeus.com>
2021-05-18 17:05:29 -0400

    Remove nested describe block

--- src/utils/noteUtils.test.js
+++ src/utils/noteUtils.test.js
@@ -1,28 +1,25 @@
 import {hasAccidental, splitNoteOctave} from './noteUtils'
 
-describe('noteUtils', () => {
-    describe('hasAccidental',  () => {
-        it('should be true when flat', () => {
-            expect(hasAccidental('Bb')).toBe(true);
-        });
-
-        it('should be true when sharp', () => {
-            expect(hasAccidental('C#')).toBe(true);
-        });
-
-        it('should be false when neither sharp nor flat', () => {
-            expect(hasAccidental('A')).toBe(false);
-        });
+describe('hasAccidental',  () => {
+    it('should be true when flat', () => {
+        expect(hasAccidental('Bb')).toBe(true);
     });
 
-    describe('splitNoteOctave', () => {
-        it('should split natural correctly', () => {
-            expect(splitNoteOctave('E5')).toStrictEqual(['E', '5']);
-        });
-
-        it('should split accidental correctly', () => {
-            expect(splitNoteOctave('C#4')).toStrictEqual(['C#', '4']);
-        });
+    it('should be true when sharp', () => {
+        expect(hasAccidental('C#')).toBe(true);
     });
 
+    it('should be false when neither sharp nor flat', () => {
+        expect(hasAccidental('A')).toBe(false);
+    });
+});
+
+describe('splitNoteOctave', () => {
+    it('should split natural correctly', () => {
+        expect(splitNoteOctave('E5')).toStrictEqual(['E', '5']);
+    });
+
+    it('should split accidental correctly', () => {
+        expect(splitNoteOctave('C#4')).toStrictEqual(['C#', '4']);
+    });
 });
