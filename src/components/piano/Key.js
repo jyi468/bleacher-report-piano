e12a5018710986beca87f352b187db7b35605bf3
jyi <joshua.yi@amadeus.com>
2021-05-18 18:47:45 -0400

    Invert key colors when pressed for 1 second. Clear timeout if pressed again

--- src/components/piano/Key.js
+++ src/components/piano/Key.js
@@ -1,10 +1,32 @@
 import './Piano.css';
+import React, {useEffect, useState} from 'react';
 import {hasAccidental} from '../../utils/noteUtils';
 
 const Key = ({note, octave}) => {
     const color = hasAccidental(note) ? 'black' : 'white';
+    const [invert, setInvert] = useState(false);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setInvert(false);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [invert]);
+
+    const handleOnClick = () => {
+        setInvert(true);
+        // Log using context
+    };
+
+    const renderClasses = () => {
+        return `key ${color} ${note} ${octave} ${invert ? 'invert' : ''}`
+    };
+
     return (
-        <div className={`key ${color} ${note} ${octave}`}>
+        <div onClick={handleOnClick} className={renderClasses()}>
             <h3 className="note">{note}</h3>
         </div>
     );
